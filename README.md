7d48a354331bb64b4a4e0dfa823159366ace9c28
mytrylin <mytrylin@staff.1111.com.tw>
2024-12-30 17:39:14 +0800

    fix: nuxt.config.ts @import "@/assets/stylesheets/_variables.scss" => @use "@/assets/stylesheets/variables" as *;

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # NUXT3 - Day2 - 專案引入 CSS 樣式
 
 
+安裝SASS
+
+> npm install -D sass
+
+
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,7 @@ export default defineNuxtConfig({
       preprocessorOptions: {
         scss: {
           additionalData: `
-            @import "@/assets/stylesheets/_variables.scss";
+            @use "@/assets/stylesheets/variables" as *;
           `,
         },
       },
